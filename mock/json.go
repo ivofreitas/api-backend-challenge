d8f7882779128f4955a5284caafdf2c3c82b9217
Ivo <ivorfn@gmail.com>
2023-06-26 00:18:22 -0300

    fix tests

--- mock/json.go
+++ mock/json.go
@@ -9,14 +9,9 @@ type JsonMock struct {
 func (m *JsonMock) Marshal(v any) ([]byte, error) {
 	args := m.Called(v)
 
-	var resultError error
 	if args.Get(1) != nil {
-		resultError = args.Get(1).(error)
+		return nil, args.Get(1).(error)
 	}
 
-	if result, ok := args.Get(0).([]byte); ok {
-		return result, nil
-	}
-
-	return nil, resultError
+	return args.Get(0).([]byte), nil
 }
